adb39ee9edf6466a41e07fcd57acb70dbe8a114d
Andrea Borruso <aborruso@gmail.com>
2020-11-15 11:36:54 +0100

    update

--- script/commentiChange.sh
+++ script/commentiChange.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+### requisiti ###
+# Miller https://github.com/johnkerl/miller
+### requisiti ###
+
 set -x
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -8,9 +12,9 @@ mkdir -p "$folder"/rawdata
 
 rm "$folder"/rawdata/commnenti.jsonl
 
-# scarica
 check="false"
 offset=0
+# finché non sei all'ultima pagina dei commenti, scarica dati
 while [[ "$check" == "false" ]]; do
   # scarica dati
   curl -kL "https://www.change.org/api-proxy/-/comments?offset="$offset"&commentable_type=Event&commentable_id=25684771" | jq . >"$folder"/rawdata/data.json
@@ -22,9 +26,12 @@ while [[ "$check" == "false" ]]; do
   let offset=offset+10
 done
 
+# converti il jsonline in CSV
 mlr --j2c cut -f id,comment,user:id,user:first_name,user:last_name,created_at,user:city,user:country_code,user:description,user:photo:url then unsparsify "$folder"/rawdata/commnenti.jsonl >"$folder"/rawdata/commnentiChange.csv
 
 mv "$folder"/rawdata/commnentiChange.csv "$folder"/../risorse/commnentiChange.csv
+mv "$folder"/rawdata/commnenti.jsonl "$folder"/../risorse/commnentiChange.jsonl
 
+# cancella file temporanei di lavoro
 rm "$folder"/rawdata/commnenti.jsonl
 rm "$folder"/rawdata/data.json
