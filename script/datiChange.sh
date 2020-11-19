2998847d1f77c4c956b7efd7931037d72f5a43bb
Andrea Borruso <aborruso@gmail.com>
2020-11-19 10:41:20 +0100

    scarica dati firmatari ogni due ore

---
+++ script/datiChange.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+### requisiti ###
+# Miller https://github.com/johnkerl/miller
+### requisiti ###
+
+set -x
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p "$folder"/rawdata
+
+URL="https://www.change.org/api-proxy/-/petitions/25684771"
+
+# leggi la risposta HTTP del sito
+code=$(curl -s -L -o /dev/null -w "%{http_code}" ''"$URL"'')
+
+# se il sito è raggiungibile scarica i dati
+if [ $code -eq 200 ]; then
+
+  curl -kLs "$URL" | jq -r '.total_signature_count' >"$folder"/../risorse/firmatariChange
+
+fi
